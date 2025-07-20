113d8a1e76a3ef61c5aadf34e0943c092a004eb7
DeepikGit <dpkashrm@gmail.com>
2025-07-20 19:14:42 +0530

    added private_subnet_module

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -11,3 +11,11 @@ module "public_subnet_01" {
   availability_zone = "ap-northeast-3a"
   vpc_id            = module.vpc.vpc_id
 }
+
+module "private_subnet_01" {
+  source            = "./modules/private_subnet"
+  private_subnet_name = "onmo-private-subnet"
+  cidr_block = "10.11.2.0/24"
+  availability_zone = "ap-northeast-3a"
+  vpc_id = module.vpc.vpc_id
+}
